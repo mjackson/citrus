3a55efedc8235b62629aa45097e61f72d442c96d
Michael Jackson <mjijackson@gmail.com>
2010-05-12 07:35:47 -0600

    Testing uses citrus/debug for convenience

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,7 @@ lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'test/unit'
-require 'citrus'
+require 'citrus/debug'
 
 class Test::Unit::TestCase
   include Citrus
