86aea401d491e838d39af05bf246f4fad965f995
Michael Jackson <mjijackson@gmail.com>
2010-06-08 16:20:05 -0600

    Updating docs

--- lib/citrus.rb
+++ lib/citrus.rb
@@ -1,6 +1,6 @@
 # Citrus is a compact and powerful parsing library for Ruby that combines the
 # elegance and expressiveness of the language with the simplicity and power of
-# parsing expression grammars.
+# parsing expressions.
 #
 # http://github.com/mjijackson/citrus
 module Citrus
