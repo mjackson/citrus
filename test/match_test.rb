db38e09f17a7c79a2e3e46e82da9f71946350ddf
Michael Jackson <mjijackson@gmail.com>
2010-10-24 16:38:11 -0700

    Separating out debug tests

--- test/match_test.rb
+++ test/match_test.rb
@@ -53,20 +53,4 @@ class MatchTest < Test::Unit::TestCase
     assert_equal(15, match.find(:alpha).length)
   end
 
-  def test_offset
-    match = Words.parse('one two')
-    assert(match)
-    assert_equal(0, match.offset)
-
-    words = match.find(:word)
-    assert(match)
-    assert_equal(2, words.length)
-
-    assert_equal('one', words[0])
-    assert_equal(0, words[0].offset)
-
-    assert_equal('two', words[1])
-    assert_equal(4, words[1].offset)
-  end
-
 end
