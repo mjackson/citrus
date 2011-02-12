70f039b017ad73aeb6d95ba62b0156c8cd3abe2f
Joachim Mårtensson <joachimmartensson@gmail.com>
2011-02-12 18:11:54 +0100

    Remember to update the input position when restoring a memoized rule from the cache. Also update tests to verify this behavior.

--- lib/citrus.rb
+++ lib/citrus.rb
@@ -308,7 +308,11 @@ module Citrus
 
       if memo[position]
         @cache_hits += 1
-        events.concat(memo[position])
+        c = memo[position]
+        unless c.empty?
+          events.concat(c)
+          self.pos += events[-1]
+        end
       else
         index = events.size
         rule.exec(self, events)
--- test/memoized_input_test.rb
+++ test/memoized_input_test.rb
@@ -26,7 +26,7 @@ class MemoizedInputTest < Test::Unit::TestCase
   def test_cache_hits1
     input = MemoizedInput.new('a')
     input.exec(LetterA.rule(:top))
-    assert_equal(2, input.cache_hits)
+    assert_equal(3, input.cache_hits)
   end
 
   def test_cache_hits2
@@ -40,4 +40,25 @@ class MemoizedInputTest < Test::Unit::TestCase
     input.exec(LetterA.rule(:top))
     assert_equal(0, input.cache_hits)
   end
+  
+  grammar :LettersABC do
+    rule :top do
+      any(all(:a,:b,:c), all(:b,:a,:c), all(:b,:c,:a))
+    end
+    rule :a do
+      "a"
+    end
+    rule :b do
+      "b"
+    end
+    rule :c do
+      "c"
+    end
+  end
+
+  def test_memoization
+    match = LettersABC.parse('bca',{:memoize=>true})
+    assert_equal('bca',match)
+  end 
+
 end
