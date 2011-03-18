ace8c8261317cc7b3f921df22ac2ed49b605ffc8
Michael Jackson <mjijackson@gmail.com>
2011-03-18 09:35:50 -0600

    Added URI example

---
+++ examples/uri_test.rb
@@ -0,0 +1,58 @@
+examples = File.expand_path('..', __FILE__)
+$LOAD_PATH.unshift(examples) unless $LOAD_PATH.include?(examples)
+
+# This file contains a suite of tests for the UniformResourceIdentifier grammar
+# found in uri.citrus.
+
+require 'citrus'
+Citrus.require 'uri'
+require 'test/unit'
+
+class UniformResourceIdentifierTest < Test::Unit::TestCase
+  U = UniformResourceIdentifier
+
+  def test_uri
+    match = U.parse('http://www.example.com')
+    assert(match)
+  end
+
+  def test_uri_with_query_string
+    match = U.parse('http://www.example.com/?q=some+query')
+    assert(match)
+  end
+
+  def test_authority
+    match = U.parse('michael@', :root => :authority)
+    assert(match)
+  end
+
+  def test_host
+    match = U.parse('127.0.0.1', :root => :host)
+    assert(match)
+
+    match = U.parse('[12AD:34FC:A453:1922::]', :root => :host)
+    assert(match)
+  end
+
+  def test_userinfo
+    match = U.parse('michael', :root => :userinfo)
+    assert(match)
+
+    assert_raise(Citrus::ParseError) do
+      U.parse('michael@', :root => :userinfo)
+    end
+  end
+
+  def test_ipliteral
+    match = U.parse('[12AD:34FC:A453:1922::]', :root => :'IP-literal')
+    assert(match)
+  end
+
+  def test_ipvfuture
+    match = U.parse('v1.123:456:789', :root => :IPvFuture)
+    assert(match)
+
+    match = U.parse('v5A.ABCD:1234', :root => :IPvFuture)
+    assert(match)
+  end
+end
