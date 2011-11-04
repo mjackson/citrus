e362e8a16daab8ede23e887849b641a905817381
Michael Jackson <mjijackson@gmail.com>
2011-11-04 10:34:40 -0700

    Version 2.4.1

--- lib/citrus/version.rb
+++ lib/citrus/version.rb
@@ -1,6 +1,6 @@
 module Citrus
   # The current version of Citrus as [major, minor, patch].
-  VERSION = [2, 4, 0]
+  VERSION = [2, 4, 1]
 
   # Returns the current version of Citrus as a string.
   def self.version
