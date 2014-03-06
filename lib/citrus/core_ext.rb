58ec7aea4998335dab09565b27c8e0fade2329c4
Bernard Lambeau <blambeau@gmail.com>
2014-03-06 07:45:44 +0100

    Move core extensions to lib/core_ext.rb

---
+++ lib/citrus/core_ext.rb
@@ -0,0 +1,18 @@
+class Object
+  # A sugar method for creating Citrus grammars from any namespace.
+  #
+  #     grammar :Calc do
+  #     end
+  #
+  #     module MyModule
+  #       grammar :Calc do
+  #       end
+  #     end
+  #
+  def grammar(name, &block)
+    namespace = respond_to?(:const_set) ? self : Object
+    namespace.const_set(name, Citrus::Grammar.new(&block))
+  rescue NameError
+    raise ArgumentError, "Invalid grammar name: #{name}"
+  end
+end
