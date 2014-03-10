7c39f101428b65ae4b3ba7c40364394a6973ee14
Bernard Lambeau <blambeau@gmail.com>
2014-03-10 16:26:24 +0100

    Only require citrus/core_ext in test helpers

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,6 @@ lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'test/unit'
-require 'citrus'
 require 'citrus/core_ext'
 
 class Test::Unit::TestCase
