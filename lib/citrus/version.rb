717dae2ce31672283212c8cbef7e52a478e61dfa
Bernard Lambeau <blambeau@gmail.com>
2014-03-13 21:03:27 +0100

    Releasing 2.5.0

--- lib/citrus/version.rb
+++ lib/citrus/version.rb
@@ -1,6 +1,6 @@
 module Citrus
   # The current version of Citrus as [major, minor, patch].
-  VERSION = [2, 4, 1]
+  VERSION = [2, 5, 0]
 
   # Returns the current version of Citrus as a string.
   def self.version
